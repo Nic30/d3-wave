c145ed325d99faffa6ad5a4b05d77bb8bb5032d5
Nic30 <nic30@seznam.cz>
2020-07-13 23:28:37 +0200

    dep name fix

--- rollup.config.js
+++ rollup.config.js
@@ -18,12 +18,13 @@ export default {
     format: 'umd',
     globals: {
 	  // lib name: name where lib exports itself on "window"
-      "d3-drag"     : "d3", 
-      "d3-shape"    : "d3",
-      "d3-hierarchy": "d3",
-      "d3-scale"    : "d3",
-      "d3-zoom"     : "d3",
       "d3-axis"     : "d3",
+      "d3-drag"     : "d3", 
+      "d3-hierarchy": "d3",
+      "d3-selection": "d3",
+      "d3-scale"    : "d3",
+      "d3-shape"    : "d3",
+      "d3-zoom"     : "d3",
       '@fortawesome/free-solid-svg-icons': 'free-solid-svg-icons',
     },
     name: 'd3'
