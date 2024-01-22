442cb7d2acd72a8375dc8dd4956211eccc1a2d3d
Nic30 <nic30@seznam.cz>
2024-01-22 10:46:15 +0100

    fix: rm relict .js in imports

--- src/rowRenderers/struct.ts
+++ src/rowRenderers/struct.ts
@@ -1,5 +1,5 @@
-import { WaveGraphSignalTypeInfo } from '../data.js';
-import { RowRendererBase } from './base.js';
+import { WaveGraphSignalTypeInfo } from '../data';
+import { RowRendererBase } from './base';
 
 /**
  * Reneers nothig, it is used for hierarchical signals as a parent container.
